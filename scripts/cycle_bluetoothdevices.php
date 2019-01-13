37cfd1257776c04054c6dcf7f264c2ab9f9392bb
Alexandr-logrus <nechaev.alex@gmail.com>
2019-01-13 11:08:50 +0500

    Update cycle_bluetoothdevices.php
    Добавлено условие и увеличен интервал записи времени прохождения цикла

--- scripts/cycle_bluetoothdevices.php
+++ scripts/cycle_bluetoothdevices.php
@@ -25,17 +25,23 @@ $bluetoothdevices_module = new bluetoothdevices();
 echo date('Y/m/d H:i:s').' Running bluetooth scanner'.PHP_EOL;
 
 $scan_time = 0;
+$latest_check = 0;
+$check_period = 10;
 
 while(true) {
-    
+
+	if ((time() - $latest_check) >= $check_period) {
+		$latest_check = time();
+		setGlobal((str_replace('.php', '', basename(__FILE__))).'Run', time(), 1);
+	}
+
 	$bluetoothdevices_module->getConfig();
-	setGlobal((str_replace('.php', '', basename(__FILE__))).'Run', time(), 1);
-	
+
 	if(time()-$scan_time > intval($bluetoothdevices_module->config['scanInterval'])) {
 		$scan_time = time();
 		$bluetoothdevices_module->processCycle();
 	}
-	
+
 	if(file_exists('./reboot') || isset($_GET['onetime'])) {
 		$db->Disconnect();
 		exit;
